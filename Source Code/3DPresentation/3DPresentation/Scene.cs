1caab743656039e774272a3009025439a0f93452
hoangminh89 <hoangminh89@gmail.com>
2011-05-26 12:31:58 +0000

    Chuyển phần code dựng XNA sang Silverlight 5 Để mỏ được cần :  - Cài Visual Studio 2010 SP1  - Cài Silverlight 5 Tools Phiên bản này vẫn chưa chạy được : do MaxVertexIndex = 2^16, không đủ dùng vì số lượng vertex 640*480 >> 2^16 Phiên bản sau, sẽ dùng kĩ thuật khác để giải quyết  - Dùng DrawIndexPrimitive : Draw nhiều lần, tăng baseVertexIndex dần + dùng QuadTree để tổ chức mảng Vertex sao cho những vùng liên quan, nằm gần nhau (các vấn đề liên quan terrain, LOD ...)  - Dùng DrawPrimitive : mỗi tam giác dùng 3 Vertex riêng => số lượng Vertex tăng lên nhiều hơn mức cần thiết... giải pháp tạm thời

---
+++ Source Code/3DPresentation/3DPresentation/Scene.cs
@@ -0,0 +1,55 @@
+﻿// ===================================================================================
+//  Copyright (c) Microsoft Corporation.  All rights reserved.
+//  THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+//  OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+//  LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+//  FITNESS FOR A PARTICULAR PURPOSE.
+// ===================================================================================
+using System;
+using System.Net;
+using System.Windows;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _3DPresentation
+{
+    /// <summary>
+    /// Represents the 3D scene. Manages the camera (view / projection)
+    /// transforms as well as a single Cube.
+    /// </summary>
+    public class Scene
+    {
+        Matrix view; // The view or camera transform
+        Matrix projection; // The projection transform to convert 3D space to 2D screen space
+
+        // The single Cube at the root of the scene
+        SceneModel Cube = new SceneModel(false);
+
+        public Scene()
+        {
+            Vector3 cameraPosition = new Vector3(0, 0, 5.0f); // the camera's position
+            Vector3 cameraTarget = Vector3.Zero; // the place the camera is looking (towards world origin)
+
+            // the transform representing a camera at a position looking at a target
+            view = Matrix.CreateLookAt(cameraPosition, cameraTarget, Vector3.Up);
+        }
+
+        public float AspectRatio
+        {
+            set
+            {
+                // update the screen space transform every time the aspect ratio changes
+                projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, value, 1.0f, 100.0f);
+            }
+        }
+
+        public void Draw(GraphicsDevice graphicsDevice, TimeSpan totalTime)
+        {
+            // clear the existing render target
+            graphicsDevice.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.Transparent, 1.0f, 0);
+
+            // draw the Cube
+            Cube.Draw(graphicsDevice, totalTime, view * projection);
+        }
+    }
+}
