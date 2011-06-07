1eac47d5068b1170cbf6c4c70014b165c29cc5a3
hoangminh89 <hoangminh89@gmail.com>
2011-06-07 14:46:39 +0000

    Thêm control đọc file ở local

---
+++ Source Code/3DPresentation/3DPresentation/OpenFileControl.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace _3DPresentation
+{
+    public partial class OpenFileControl : UserControl
+    {
+        public OpenFileControl()
+        {
+            InitializeComponent();
+            btOpen.Click += new RoutedEventHandler(btOpen_Click);
+        }
+
+        void btOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+            dialog.Filter = "Text|*.txt|All Files|*.*";
+            if(dialog.ShowDialog() == true)
+            {
+                Dispatcher.BeginInvoke(new Action(() => { lbContent.Content = dialog.File.Name; }));
+                //using (StreamReader reader = dialog.File.OpenText()) 
+                //{
+                //    // Store file content in 'text' variable
+                //    string text = reader.ReadToEnd();
+                //}
+                using (Stream stream = dialog.File.OpenRead())
+                {
+                    // Store file content in 'text' variable
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(stream);
+                    img.Source = bitmapImage;
+                }
+            }
+        }
+    }
+}
