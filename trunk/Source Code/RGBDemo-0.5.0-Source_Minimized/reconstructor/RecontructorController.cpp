87980c16c9e98229b04dfa1ac2b5b2c7547e1c66
tntuan0712494 <tntuan0712494@a2cfd237-d426-27f4-20f1-81bcd826f934>
2011-06-28 02:49:54 +0000

    refacor 3 mode lưu file

---
+++ trunk/Source Code/RGBDemo-0.5.0-Source_Minimized/reconstructor/RecontructorController.cpp
@@ -0,0 +1,123 @@
+#include "RecontructorController.h"
+
+RecontructorController::RecontructorController(void)
+{
+	m_iSavePlyMode = 0;
+	//SetSaveFilePlyMode(RecontructorController::Flags::DecreaseSameVertex, true);
+}
+
+RecontructorController::~RecontructorController(void)
+{
+}
+
+void RecontructorController::Run()
+{
+	if (m_bIsFromKineck)
+	{
+		RunFromKineck();
+	}
+	else
+	{
+		RunFromRecordedData();
+	}
+	
+}
+
+void RecontructorController::RunFromKineck()
+{
+	ntk_debug_level = 1;
+	// Display the number of processors/cores
+	cout<<boost::thread::hardware_concurrency()
+		<<" processors/cores detected."<<endl<<endl;
+	cout<<"When threads are running, press enter to stop"<<endl;
+
+	// The number of producers/consumers
+	int nrProducers, nrConsumers;
+
+	// The shared queue
+	SynchronisedQueue<RGBDImage *> queue;
+	queue.SetMaxQueueElement(1);
+
+	nrProducers = 1;
+
+	nrConsumers = 1;
+	FindFrameConsumer::Init();
+
+	// Create producers
+	boost::thread_group producers;
+	FileGrabberProducer p(0, &queue);
+	p.initialize();
+	producers.create_thread(p);
+
+	// Create consumers
+	boost::thread_group consumers;
+	FindFrameConsumer c(0, &queue, m_strRecordedFolderData.c_str(), 0);
+	c.SetSaveRawData(m_bIsSaveRawData);
+	c.SetSaveMappedData(m_bIsSaveMappedData);
+
+		c.SetDestinationFolder(m_strDestinationFolder);
+	c.SetRecordedFolderData(m_strRecordedFolderData);
+	c.SetPathCalibrationData(m_strPathCalibrationData);
+
+	consumers.create_thread(c);
+
+	//FIX ME: change this to sth like check signal end this thread
+	// Wait for enter (two times because the return from the 
+	// previous cin is still in the buffer)
+	getchar(); getchar();
+
+	// Interrupt the threads and stop them
+	producers.interrupt_all(); producers.join_all();
+	consumers.interrupt_all(); consumers.join_all();
+}
+
+void RecontructorController::RunFromRecordedData()
+{
+	ntk_debug_level = 1;
+	// Display the number of processors/cores
+	cout<<boost::thread::hardware_concurrency()
+		<<" processors/cores detected."<<endl<<endl;
+	cout<<"When threads are running, press enter to stop"<<endl;
+
+	// The number of producers/consumers
+	int nrProducers, nrConsumers;
+
+	// The shared queue
+	SynchronisedQueue<RGBDImage *> queue;
+	queue.SetMaxQueueElement(1);
+
+	nrProducers = 1;
+
+	nrConsumers = 1;
+	FindFrameConsumer::Init();
+
+	// Create producers
+	boost::thread_group producers;
+	ModeRecordGrabberProducer p(0, &queue, m_strRecordedFolderData);
+	ntk::RGBDCalibration* calib_data = new RGBDCalibration();
+	calib_data->loadFromFile(m_strPathCalibrationData.c_str());
+	p.setCalibration(calib_data);
+	producers.create_thread(p);
+
+	// Create consumers
+	boost::thread_group consumers;
+	FindFrameConsumer c(0, &queue, m_strRecordedFolderData.c_str(), 0);
+	c.SetSaveRawData(m_bIsSaveRawData);
+	c.SetSaveMappedData(m_bIsSaveMappedData);
+
+	c.SetDestinationFolder(m_strDestinationFolder);
+	c.SetRecordedFolderData(m_strRecordedFolderData);
+	c.SetPathCalibrationData(m_strPathCalibrationData);
+	c.setFilterFlags(m_iSavePlyMode);
+
+	consumers.create_thread(c);
+
+	//FIX ME: change this to sth like check signal end this thread
+	// Wait for enter (two times because the return from the 
+	// previous cin is still in the buffer)
+	getchar(); getchar();
+
+	// Interrupt the threads and stop them
+	producers.interrupt_all(); producers.join_all();
+	consumers.interrupt_all(); consumers.join_all();
+}
---
+++ trunk/Source Code/RGBDemo-0.5.0-Source_Minimized/reconstructor/RecontructorController.h
@@ -0,0 +1,101 @@
+﻿#pragma once
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <sstream>
+#include <iomanip>
+
+#include <boost/thread.hpp>
+#include <string>
+#include <sstream>
+#include <ntk/SynchronisedQueue.h>
+#include <ntk/FileGrabberProducer.h>
+#include <ntk/FindFrameConsumer.h>
+#include <ntk/ModeRecordGrabberProducer.h>
+
+using namespace std;
+using namespace boost;
+using namespace boost::this_thread;
+using namespace ntk;
+using namespace cv;
+
+//how to use thread in boost library
+//http://www.quantnet.com/cplusplus-multithreading-boost/
+//http://en.highscore.de/cpp/boost/
+
+//how to fix error when linking to thread boost library
+//https://svn.boost.org/trac/boost/ticket/2226
+namespace boost {
+	struct thread::dummy {};
+}
+
+class RecontructorController
+{
+public:
+		enum Flags {
+		NotDecreaseSameVertex = 0x1,
+		DecreaseSameVertex = 0x2,
+		Notprocess = 0x4
+		//SaveFinalPly = 0x8
+	};
+	//từ kineck/từ file --> save/ko save data
+	//save data gồm raw và mapped
+	//xuất ra ply chưa bỏ trùng/đã bỏ trùng/chưa hề align
+	//xuất ra pairs 3d cho frame đó
+	//có xuất kết quả ply tổng hợp cuối cùng
+	//có dùng thêm icp
+private:
+	bool m_bIsFromKineck;
+	bool m_bIsSaveRawData;
+	bool m_bIsSaveMappedData;
+
+
+	int m_iSavePlyMode;
+
+	bool m_bSavePairs;
+	bool m_bSaveFinalPly;
+	bool m_bUseICP;
+
+	//get arg data
+	//1. destination folder data, we will create a temperate folder to save data 
+	//		before copy to this folder
+	//2. recorded folder data, in this mode, do not save data back
+	//3. path of calibration data
+	string m_strDestinationFolder;
+	string m_strRecordedFolderData;
+	string m_strPathCalibrationData;
+
+public: 
+	void SetLoadDataFromKineck(bool b) {
+		m_bIsFromKineck = b;
+		if (m_bIsFromKineck)
+		{
+			SetSaveRawData(true);
+			SetSaveMappedData(false);
+		}
+		else
+		{
+			SetSaveRawData(false);
+			SetSaveMappedData(false);
+		}
+	}
+
+	//normal noneed, setup in upper function
+	void SetSaveRawData(bool b) {m_bIsSaveRawData = b;}
+	void SetSaveMappedData(bool b) {m_bIsSaveMappedData = b;}
+	void SetSaveFilePlyMode(Flags flag, bool enabled)
+	{ if (enabled) m_iSavePlyMode |= flag; else m_iSavePlyMode &= ~flag; }
+
+	void SetDestinationFolder(string str) {m_strDestinationFolder = str;}
+	void SetRecordedFolderData(string str) {m_strRecordedFolderData = str;}
+	void SetPathCalibrationData(string str) {m_strPathCalibrationData = str;}
+public:
+	RecontructorController(void);
+	~RecontructorController(void);
+
+	void GetArgData(char argc, char** argv);
+	void Run();
+	void RunFromKineck();
+	void RunFromRecordedData();
+};
