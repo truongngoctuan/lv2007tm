b7074b446f1afe0cb5e0a4597bfb3962828dc5ef
tntuan0712494 <tntuan0712494@a2cfd237-d426-27f4-20f1-81bcd826f934>
2011-06-29 15:34:00 +0000

    kích hoạt sl hoàn toàn từ c++:
     - click button download package: down file zip(nén file bin +dll +... của rgb-recontructor), giải nén vào WorkingDirectory(ổ d:\\)
     - kích hoạt file exe, truyền tham số,...
    
    phải dùng file nén vì không có cơ chế đọc list file từ thư mục CLientBin 
    cập nhật file .zip trong ClientBin project Presentation.Web
    
    cần:
    chỉnh lại các thông số của bên c++ dùng hoàn toàn absolute path
    chỉnh lại để proejct bên c++ build ra vào ClientBin

---
+++ trunk/Source Code/PointPresentation/3DPresentation/ClientFileAndDirectory.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices.Automation;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using SharpGIS;
+
+namespace _3DPresentation
+{
+    public class ClientFileAndDirectory
+    {//http://justinangel.net/CuttingEdgeSilverlight4ComFeatures
+        //using (dynamic fsoCom = AutomationFactory.CreateObject("Scripting.FileSystemObject"))
+        //{
+        //    dynamic file = fsoCom.OpenTextFile(@"c:\test.txt", 1, true);
+        //    MessageBox.Show(file.ReadAll());
+        //    file.Close();
+        //}
+
+        public static bool FileExists(string strFolderName)
+        {
+            using (dynamic fsoCom = AutomationFactory.CreateObject("Scripting.FileSystemObject"))
+            {
+                return fsoCom.FileExists(strFolderName);
+            }
+        }
+
+        public static bool FolderExists(string strFolderName)
+        {
+            using (dynamic fsoCom = AutomationFactory.CreateObject("Scripting.FileSystemObject"))
+            {
+                return fsoCom.FolderExists(strFolderName);
+            }
+        }
+
+        public static void CreateFolder(string strFolderName)
+        {
+            using (dynamic fsoCom = AutomationFactory.CreateObject("Scripting.FileSystemObject"))
+            {
+                fsoCom.CreateFolder(strFolderName);
+            }
+        }
+
+        public static void DeleteFolder(string strFolderName)
+        {
+            using (dynamic fsoCom = AutomationFactory.CreateObject("Scripting.FileSystemObject"))
+            {
+                dynamic flr = fsoCom.GetFolder(strFolderName);
+                fsoCom.DeleteFolder(flr);
+            }
+        }
+
+        public static void DeleteFile(string strFolderName)
+        {
+            using (dynamic fsoCom = AutomationFactory.CreateObject("Scripting.FileSystemObject"))
+            {
+                dynamic fl = fsoCom.GetFile(strFolderName);
+                fsoCom.DeleteFile(fl);
+            }
+        }
+
+        private static void ReadWriteStream(Stream readStream, Stream writeStream)
+        {
+            int Length = 256;
+            Byte[] buffer = new Byte[Length];
+            int bytesRead = readStream.Read(buffer, 0, Length);
+            // write the required bytes
+            while (bytesRead > 0)
+            {
+                writeStream.Write(buffer, 0, bytesRead);
+                bytesRead = readStream.Read(buffer, 0, Length);
+            }
+            readStream.Close();
+            writeStream.Close();
+        }
+
+        public static void UnZip(string strWorkingDirectory, Stream sUnzip)
+        {
+            try
+            {
+                UnZipper unzip = new UnZipper(sUnzip);
+                    //string str = string.Empty;
+                    //str += "___DIRECTORIES:___\n";
+                    foreach (string filename in unzip.DirectoriesInZip)
+                    {
+                        //str += filename + "\n";
+                        if (ClientFileAndDirectory.FolderExists(strWorkingDirectory + filename))
+                        {
+                            ClientFileAndDirectory.DeleteFolder(strWorkingDirectory + filename);
+                        }
+                        ClientFileAndDirectory.CreateFolder(strWorkingDirectory + filename);
+                    }
+
+                    //str += "___FILES:___\n";
+                    foreach (string filename in unzip.FileNamesInZip)
+                    {
+                        //str += filename + "\n";
+                        if (ClientFileAndDirectory.FileExists(strWorkingDirectory + filename))
+                        {
+                            ClientFileAndDirectory.DeleteFile(strWorkingDirectory + filename);
+                        }
+                        Stream stream = unzip.GetFileStream(filename);
+                        string saveTo = strWorkingDirectory + filename;
+                        // create a write stream
+                        FileStream writeStream = new FileStream(saveTo, FileMode.Create, FileAccess.Write);
+                        // write to the stream
+                        ReadWriteStream(stream, writeStream);
+                        writeStream.Close();
+
+                    }
+                    //MessageBox.Show(str);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ trunk/Source Code/PointPresentation/3DPresentation/ClientPackage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace _3DPresentation
+{
+    public class ClientPackage
+    {
+        private string _strWorkingDirectory;
+
+        public string WorkingDirectory
+        {
+            get { return _strWorkingDirectory; }
+            set { _strWorkingDirectory = value; }
+        }
+
+        public void DownloadtoClient(string strPackageUri, string strWorkingDirectory)
+        {
+            //http://www.developerfusion.com/code/4669/save-a-stream-to-a-file/
+            //http://www.sharpgis.net/post/2010/08/25/REALLY-small-unzip-utility-for-Silverlight-e28093-Part-2.aspx
+            WebClient client = new WebClient();
+
+            client.DownloadProgressChanged += client_DownloadProgressChanged;
+            client.OpenReadCompleted += client_OpenReadCompleted;
+            Uri ur = Application.Current.Host.Source;
+            int a = ur.AbsolutePath.LastIndexOf('/');
+            string asd1 = ur.AbsolutePath.Substring(0, a);
+            string asd = asd1 + strPackageUri;
+
+            WorkingDirectory = strWorkingDirectory;
+
+            client.OpenReadAsync(new Uri(asd, UriKind.Relative));
+        }
+
+        //private decimal _megaBytesReceived;
+        //private decimal _totalMegaBytes;
+        void client_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //_megaBytesReceived = e.BytesReceived / 1024m / 1024m;
+            //_totalMegaBytes = e.TotalBytesToReceive / 1024m / 1024m;
+            //_megaBytesReceived = Math.Round(_megaBytesReceived, 3);
+            //_totalMegaBytes = Math.Round(_totalMegaBytes, 3);
+            //txtMegabytes.Text = string.Format("{0} von {1}", _megaBytesReceived, _totalMegaBytes);
+            //progressBar.Value = e.ProgressPercentage;
+        }
+
+        void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+
+            if (e.Error != null)
+            {
+                // TODO: Fehler anzeigen
+            }
+            else
+            {
+                ClientFileAndDirectory.UnZip(WorkingDirectory, e.Result);
+            }
+        }
+    }
+}
