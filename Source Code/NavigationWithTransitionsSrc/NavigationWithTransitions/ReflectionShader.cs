0315c8bc10cd501b5973b7d50757b18bfbef0ca9
tntuan0712494 <tntuan0712494@a2cfd237-d426-27f4-20f1-81bcd826f934>
2011-07-07 14:09:05 +0000

    project làm phần giao diện cơ bản editor, gốm 3 phần cơ bản: top: play, stop, new model (chạy lại) left: toolbox có chứa các chức năng bottom: hiển thị các frame nhận đc mid: hiển thị model

---
+++ Source Code/NavigationWithTransitionsSrc/NavigationWithTransitions/ReflectionShader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows.Media.Effects;
+using System.Windows;
+
+namespace NavigationWithTransitions
+{
+  public class ReflectionShader : ShaderEffect
+  {
+    static PropertyChangedCallback ElementHeightRegisterCallback;
+
+    static ReflectionShader()
+    {
+      ElementHeightRegisterCallback = ShaderEffect.PixelShaderConstantCallback(1);
+    }
+
+    public ReflectionShader()
+    {
+        //Uri u = new Uri(@"/NavigationWithTransitions;component/Reflection.ps", UriKind.Relative);
+        Uri u = new Uri(@"/NavigationWithTransitions;component/Reflection.ps", UriKind.Relative);
+      PixelShader = new PixelShader() { UriSource = u };
+      base.UpdateShaderValue(ElementHeightProperty);
+    }
+
+    public static readonly DependencyProperty ElementHeightProperty =
+            DependencyProperty.Register("ElementHeight",
+            typeof(double),
+            typeof(ReflectionShader),
+            new PropertyMetadata(100.0, OnElementHeightChanged));
+
+    static void OnElementHeightChanged(DependencyObject d, 
+      DependencyPropertyChangedEventArgs e)
+    {
+      ElementHeightRegisterCallback(d, e);
+      (d as ReflectionShader).OnElementHeightChanged(
+        (double)e.OldValue, 
+        (double)e.NewValue);
+    }
+
+    protected virtual void OnElementHeightChanged(double oldValue, double newValue)
+    {
+      PaddingBottom = newValue;
+    }
+
+    public double ElementHeight
+    {
+      get { return (double)base.GetValue(ElementHeightProperty); }
+      set { base.SetValue(ElementHeightProperty, value); }
+    }
+  }
+}
