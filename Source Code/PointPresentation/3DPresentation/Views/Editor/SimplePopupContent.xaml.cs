bdae2e533ca34be47f70bbe6c38759968dadb60a
tntuan0712494 <tntuan0712494@a2cfd237-d426-27f4-20f1-81bcd826f934>
2011-07-08 16:58:38 +0000

    poppup: chọn delete image (chưa cài đặt), set ff, set rf, open match= tay, cập nhật 2 cái index sữa lỗi selected index

---
+++ Source Code/PointPresentation/3DPresentation/Views/Editor/SimplePopupContent.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace _3DPresentation.Views.Editor
+{
+    public partial class SimplePopupContent : UserControl
+    {
+        public SimplePopupContent()
+        {
+            InitializeComponent();
+        }
+
+        EditorView _parent;
+
+        public EditorView ParentView
+        {
+            get { return _parent; }
+            set { _parent = value; }
+        }
+
+        public int imgIndex = -1;
+
+        private void btDeleteImage_Click(object sender, RoutedEventArgs e)
+        {
+            //ParentView
+            ParentView.DeleteFrame(imgIndex);
+        }
+
+        private void btSetFixedFrame_Click(object sender, RoutedEventArgs e)
+        {
+            ParentView.FixedImageIndex = imgIndex;
+        }
+
+        private void btSetReferenceFrame_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ParentView.ReferenceImageIndex = imgIndex;
+                MessageBox.Show(imgIndex.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
